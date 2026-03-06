243bfa975a4b827e7958611e3339262344c36b15
Kmk18 <22028067@vnu.edu.vn>
2026-03-07 01:27:43 +0700

    fix first flow to map

--- src/mobile/src/navigation/AppNavigator.tsx
+++ src/mobile/src/navigation/AppNavigator.tsx
@@ -4,27 +4,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { TabNavigator } from './TabNavigator';
 import { AuthNavigator } from './AuthNavigator';
 import { RescueModeScreen } from '../screens/RescueModeScreen';
-import { useAuthStore } from '../store/useAuthStore';
 
 const Stack = createNativeStackNavigator();
 
 export const AppNavigator = () => {
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {isAuthenticated ? (
-          <>
-            <Stack.Screen name="MainTabs" component={TabNavigator} />
-            <Stack.Screen name="Auth" component={AuthNavigator} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Auth" component={AuthNavigator} />
-            <Stack.Screen name="MainTabs" component={TabNavigator} />
-          </>
-        )}
+        <Stack.Screen name="MainTabs" component={TabNavigator} />
+        <Stack.Screen name="Auth" component={AuthNavigator} />
         <Stack.Screen name="RescueMode" component={RescueModeScreen} options={{ presentation: 'modal' }} />
       </Stack.Navigator>
     </NavigationContainer>
